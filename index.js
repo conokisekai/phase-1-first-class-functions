4237ec20983e0bcfa50ce1a24d4585a3a4e5601c
Conrad Kambi <kambiconrad@gmail.com>
2023-09-21 21:47:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    // Function body
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    // Function body
+  };
+}
